77983dd7f5c401159ce52aae168bd642387ca8ef
JO Yeongmu <75081608+fprh13@users.noreply.github.com>
2023-10-26 12:15:44 +0900

    [unrated] Title: 수열과 구간 쿼리 2, Time: 12.68 ms, Memory: 81.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] arr, int[][] queries) {
+        List<Integer> result = new ArrayList<>();
+
+        for (int[] query : queries) {
+            int s = query[0];
+            int e = query[1];
+            int k = query[2];
+            List<Integer> answer = new ArrayList<>();
+            for (int i = s; i < e + 1; i++) {
+                if (arr[i] > k) {
+                    answer.add(arr[i]);
+                }
+            }
+
+            if (answer.size() != 0) {
+                answer.stream()
+                    .mapToInt(i -> i)
+                    .min()
+                    .ifPresent(min -> result.add(min));
+            } else {
+                result.add(-1);
+            }
+
+        }
+
+        return result.stream().mapToInt(i->i).toArray();
+    }
+}
