538f8d51bb46f4b1ba22dc05bd16f45ad56b9efb
JO Yeongmu <75081608+fprh13@users.noreply.github.com>
2023-11-07 22:48:30 +0900

    [level unrated] Title: 9로 나눈 나머지, Time: 0.05 ms, Memory: 75.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181914. 9로 나눈 나머지/9로 나눈 나머지.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(String number) {
+        
+        int sum = 0;
+        
+        char[] charArray = number.toCharArray();
+        for (char ch : charArray) {
+            sum +=  Character.getNumericValue(ch);
+        }
+        
+        return sum % 9;
+    }
+}
